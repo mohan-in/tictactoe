ef25b6b4ce839812e877871192120701f75c8e62
gocode <mail2mohanram@gmail.com>
2016-02-24 23:26:19 +0530

    got it working

--- main.go
+++ main.go
@@ -4,71 +4,86 @@ import (
 	"fmt"
 )
 
-var turn byte = 'o'
+type State [3][3]byte
+
 var computer byte = 'o'
 var user byte = 'x'
 
 func main() {
-	var state = [3][3]byte{}
-	fmt.Println(run(state))
-}
+	var state = State{}
 
-func run(state [3][3]byte) int {
-	if isWinningState(state, 'x') {
-		fmt.Print("x wins ")
-		fmt.Println(state)
-		return 1
-	} else if isWinningState(state, 'o') {
-		fmt.Print("o wins ")
-		fmt.Println(state)
-		return -1
-	} else {
-		fmt.Println(state)
-		if turn == computer {
-			fs := getFutureStates(state, turn)
-			switchTurn()
-			for _, s := range fs {
-				return run(s)
-			}
-		} else {
-			state = getUserInput(state)
-			switchTurn()
-			run(state)
+	for {
+		ch := make(chan State, 9)
+		run(state, 0, computer, ch)
+		state = <-ch
+		printState(state)
+		if isWinningState(state, 'o') {
+			fmt.Println("Computer Wins!!")
+			break
+		}
+
+		state = getUserInput(state)
+		printState(state)
+		if isWinningState(state, 'x') {
+			fmt.Println("You Win!!")
+			break
 		}
 	}
+}
 
+func run(state State, level int, turn byte, ch chan State) int {
+	if isWinningState(state, 'x') {
+		return -1
+	} else if isWinningState(state, 'o') {
+		return 1
+	} else {
+		fs := getFutureStates(state, turn)
+		for _, s := range fs {
+			w := run(s, level+1, switchTurn(turn), ch)
+			if w == 1 {
+				//printState(s)
+				if level == 0 {
+					ch <- s
+				} else {
+					return 1
+				}
+			} else {
+				return w
+			}
+		}
+	}
 	return 0
 }
 
-func isWinningState(board [3][3]byte, t byte) bool {
+func isWinningState(state State, t byte) bool {
 	for i := 0; i < 3; i++ {
-		if board[i][0] == t && board[i][1] == t && board[i][2] == t {
+		if state[i][0] == t && state[i][1] == t && state[i][2] == t {
 			return true
 		}
 
-		if board[0][i] == t && board[1][i] == t && board[2][i] == t {
+		if state[0][i] == t && state[1][i] == t && state[2][i] == t {
 			return true
 		}
 	}
-	if (board[0][0] == t && board[1][1] == t && board[2][2] == t) ||
-		(board[0][2] == t && board[1][1] == t && board[2][0] == t) {
+	if (state[0][0] == t && state[1][1] == t && state[2][2] == t) ||
+		(state[0][2] == t && state[1][1] == t && state[2][0] == t) {
 		return true
 	}
 
 	return false
 }
 
-func getFutureStates(state [3][3]byte, turn byte) [9][3][3]byte {
-	result := [9][3][3]byte{}
-	k := 0
+func getFutureStates(state State, turn byte) []State {
+	result := []State{}
+	s := state
 
 	for i := 0; i < 3; i++ {
 		for j := 0; j < 3; j++ {
-			if state[i][j] == 0 {
-				state[i][j] = turn
-				result[k] = state
-				k++
-				break
+			if s[i][j] == 0 {
+				s[i][j] = turn
+				ts := state
+				ts[i][j] = turn
+				result = append(result, ts)
 			}
 		}
 	}
@@ -76,15 +91,15 @@ func getFutureStates(state [3][3]byte, turn byte) [9][3][3]byte {
 	return result
 }
 
-func switchTurn() {
+func switchTurn(turn byte) byte {
 	if turn == 'x' {
-		turn = 'o'
+		return 'o'
 	} else {
-		turn = 'x'
+		return 'x'
 	}
 }
 
-func getUserInput(state [3][3]byte) [3][3]byte {
+func getUserInput(state State) State {
 	var i, j int
 	fmt.Print("Enter row: ")
 	fmt.Scanf("%d", &i)
@@ -92,4 +107,14 @@ func getUserInput(state [3][3]byte) [3][3]byte {
 	fmt.Scanf("%d", &j)
 	state[i][j] = user
 	return state
+}
+
+func printState(state State) {
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			fmt.Printf("%c\t", state[i][j])
+		}
+		fmt.Println()
+	}
+	fmt.Println("==================")
 }
